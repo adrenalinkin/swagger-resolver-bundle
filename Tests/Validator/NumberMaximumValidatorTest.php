05a558d3df8ba72772e98fae3e0d3842de77562c
Viktor Linkin <adrenalinkin@gmail.com>
2021-07-19 00:18:56 +0300

    Covered `NumberMaximumValidator::validate` method

--- Tests/Validator/NumberMaximumValidatorTest.php
+++ Tests/Validator/NumberMaximumValidatorTest.php
@@ -7,6 +7,7 @@ namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Validator;
 use EXSyst\Component\Swagger\Schema;
 use Linkin\Bundle\SwaggerResolverBundle\Validator\NumberMaximumValidator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 
 class NumberMaximumValidatorTest extends TestCase
 {
@@ -60,6 +61,89 @@ class NumberMaximumValidatorTest extends TestCase
                 'type' => self::TYPE_NUMBER,
                 'maximum' => 10.99,
                 'expectedResult' => true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider failToPassValidationDataProvider
+     */
+    public function testFailToPassValidation(bool $isExclusiveMaximum, $maximum, $value): void
+    {
+        $schema = new Schema([
+            'type' => self::TYPE_INT,
+            'maximum' => $maximum,
+            'exclusiveMaximum' => $isExclusiveMaximum,
+        ]);
+
+        $this->expectException(InvalidOptionsException::class);
+
+        $this->sut->validate($schema, 'age', $value);
+    }
+
+    public function failToPassValidationDataProvider(): array
+    {
+        return [
+            'Fail with maximal int value and exclusive mode' => [
+                'isExclusiveMaximum' => true,
+                'maximum' => 10,
+                'value' => 10,
+            ],
+            'Fail with int more than maximal value' => [
+                'isExclusiveMaximum' => false,
+                'maximum' => 10,
+                'value' => 11,
+            ],
+            'Fail with maximal float value and exclusive mode' => [
+                'isExclusiveMaximum' => true,
+                'maximum' => 10.01,
+                'value' => 10.01,
+            ],
+            'Fail with float more than maximal value' => [
+                'isExclusiveMaximum' => false,
+                'maximum' => 10.1,
+                'value' => 10.1001,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider canPassValidationDataProvider
+     */
+    public function testCanPassValidation(bool $isExclusiveMaximum, $maximum, $value): void
+    {
+        $schema = new Schema([
+            'type' => self::TYPE_INT,
+            'maximum' => $maximum,
+            'exclusiveMaximum' => $isExclusiveMaximum,
+        ]);
+
+        $this->sut->validate($schema, 'age', $value);
+        self::assertTrue(true);
+    }
+
+    public function canPassValidationDataProvider(): array
+    {
+        return [
+            'Pass validation with lower than maximal int value and exclusive mode' => [
+                'isExclusiveMaximum' => true,
+                'maximum' => 10,
+                'value' => 9,
+            ],
+            'Pass validation with lower than maximal int value' => [
+                'isExclusiveMaximum' => false,
+                'maximum' => 10,
+                'value' => 10,
+            ],
+            'Pass validation with lower than maximal float value and exclusive mode' => [
+                'isExclusiveMaximum' => true,
+                'maximum' => 10.002,
+                'value' => 10.001,
+            ],
+            'Pass validation with lower than maximal float value' => [
+                'isExclusiveMaximum' => false,
+                'maximum' => 10.002,
+                'value' => 10.002,
             ],
         ];
     }
