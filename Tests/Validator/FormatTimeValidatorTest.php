e943e94b9b934711eef14c5f66efe74627ca754f
Viktor Linkin <adrenalinkin@gmail.com>
2021-08-22 13:46:00 +0300

    Added unit test for the `FormatTimeValidator::validate`

--- Tests/Validator/FormatTimeValidatorTest.php
+++ Tests/Validator/FormatTimeValidatorTest.php
@@ -16,6 +16,7 @@ namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Validator;
 use EXSyst\Component\Swagger\Schema;
 use Linkin\Bundle\SwaggerResolverBundle\Validator\FormatTimeValidator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 
 /**
  * @author Viktor Linkin <adrenalinkin@gmail.com>
@@ -58,6 +59,83 @@ class FormatTimeValidatorTest extends TestCase
             'Success with right format' => [
                 'type' => self::FORMAT_TIME,
                 'expectedResult' => true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider failToPassValidationDataProvider
+     */
+    public function testFailToPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_TIME,
+            'pattern' => $pattern,
+        ]);
+
+        $this->expectException(InvalidOptionsException::class);
+
+        $this->sut->validate($schema, 'savedAtTime', $value);
+    }
+
+    public function failToPassValidationDataProvider(): array
+    {
+        return [
+            'Fail when value with incorrect time pattern - number' => [
+                'pattern' => null,
+                'value' => '100',
+            ],
+            'Fail when value with incorrect time pattern - more than 2 digit' => [
+                'pattern' => null,
+                'value' => '100:05:55',
+            ],
+            'Fail when pattern set and value can NOT convert into DateTime' => [
+                'pattern' => 'any-string-here', // TODO: should check format
+                'value' => '10_05_45',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider canPassValidationDataProvider
+     */
+    public function testCanPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_TIME,
+            'pattern' => $pattern,
+        ]);
+
+        $this->sut->validate($schema, 'savedAtTime', $value);
+        self::assertTrue(true);
+    }
+
+    public function canPassValidationDataProvider(): array
+    {
+        return [
+            'Pass when null value' => [
+                'pattern' => null,
+                'value' => null,
+            ],
+            'Pass when empty string value' => [
+                'pattern' => null,
+                'value' => '',
+            ],
+            'Pass when empty zero value' => [ // TODO: should not pass validation
+                'pattern' => null,
+                'value' => '0',
+            ],
+            'Pass when false value' => [ // TODO: should not pass validation
+                'pattern' => null,
+                'value' => false,
+            ],
+            'Pass when value with correct time pattern' => [
+                'pattern' => null,
+                'value' => '10:05:45',
+            ],
+            'Pass when pattern set and value can convert into DateTime' => [
+                'pattern' => 'any-string-here', // TODO: should check format
+                'value' => '10/05/45',
             ],
         ];
     }
