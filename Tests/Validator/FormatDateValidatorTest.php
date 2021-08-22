fea629290dcd5b69711cd461861b99d29736aa76
Viktor Linkin <adrenalinkin@gmail.com>
2021-08-23 01:01:17 +0300

    Added unit tests for the `FormatDateValidator::supports`

---
+++ Tests/Validator/FormatDateValidatorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SwaggerResolverBundle package.
+ *
+ * (c) Viktor Linkin <adrenalinkin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Validator;
+
+use EXSyst\Component\Swagger\Schema;
+use Linkin\Bundle\SwaggerResolverBundle\Validator\FormatDateValidator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Viktor Linkin <adrenalinkin@gmail.com>
+ */
+class FormatDateValidatorTest extends TestCase
+{
+    private const FORMAT_DATE = 'date';
+
+    /**
+     * @var FormatDateValidator
+     */
+    private $sut;
+
+    protected function setUp(): void
+    {
+        $this->sut = new FormatDateValidator();
+    }
+
+
+    /**
+     * @dataProvider supportsDataProvider
+     */
+    public function testSupports(string $format, bool $expectedResult): void
+    {
+        $schema = new Schema([
+            'format' => $format,
+        ]);
+
+        $isSupported = $this->sut->supports($schema);
+
+        self::assertSame($isSupported, $expectedResult);
+    }
+
+    public function supportsDataProvider(): array
+    {
+        return [
+            'Fail with unsupported format' => [
+                'format' => '_invalid_format_',
+                'expectedResult' => false,
+            ],
+            'Success with right format' => [
+                'type' => self::FORMAT_DATE,
+                'expectedResult' => true,
+            ],
+        ];
+    }
+}
