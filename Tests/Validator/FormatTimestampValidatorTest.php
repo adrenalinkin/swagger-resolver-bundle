a40f9a6be35f1df751b9f865f43489033b8e5826
Viktor Linkin <adrenalinkin@gmail.com>
2021-08-22 14:53:39 +0300

    Added unit tests for the `FormatTimestampValidator::validate`

--- Tests/Validator/FormatTimestampValidatorTest.php
+++ Tests/Validator/FormatTimestampValidatorTest.php
@@ -16,6 +16,7 @@ namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Validator;
 use EXSyst\Component\Swagger\Schema;
 use Linkin\Bundle\SwaggerResolverBundle\Validator\FormatTimestampValidator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 
 /**
  * @author Viktor Linkin <adrenalinkin@gmail.com>
@@ -58,6 +59,83 @@ class FormatTimestampValidatorTest extends TestCase
             'Success with right format' => [
                 'type' => self::FORMAT_TIMESTAMP,
                 'expectedResult' => true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider failToPassValidationDataProvider
+     */
+    public function testFailToPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_TIMESTAMP,
+            'pattern' => $pattern,
+        ]);
+
+        $this->expectException(InvalidOptionsException::class);
+
+        $this->sut->validate($schema, 'updatedAt', $value);
+    }
+
+    public function failToPassValidationDataProvider(): array
+    {
+        return [
+            'Fail when value with incorrect timestamp pattern' => [
+                'pattern' => null,
+                'value' => '2020-10-10',
+            ],
+            'Fail when pattern set and value can NOT convert into DateTime' => [
+                'pattern' => 'any-string-here',
+                'value' => '2020-10-10 00:00:00',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider canPassValidationDataProvider
+     */
+    public function testCanPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_TIMESTAMP,
+            'pattern' => $pattern,
+        ]);
+
+        $this->sut->validate($schema, 'updatedAt', $value);
+        self::assertTrue(true);
+    }
+
+    public function canPassValidationDataProvider(): array
+    {
+        return [
+            'Pass when null value' => [
+                'pattern' => null,
+                'value' => null,
+            ],
+            'Pass when empty string value' => [
+                'pattern' => null,
+                'value' => '',
+            ],
+            'Pass when empty zero value' => [
+                'pattern' => null,
+                'value' => '0',
+            ],
+            'Pass when false value' => [
+                'pattern' => null,
+                'value' => false,
+            ],
+            'Fail when true value' => [
+                'pattern' => null,
+                'value' => true,
+            ],
+            'Pass when value with correct time pattern' => [
+                'pattern' => null,
+                'value' => '1629620000',
+            ],
+            'Pass when pattern set and value can convert into DateTime' => [
+                'pattern' => 'any-string-here', // TODO: should check format
+                'value' => '1620000000',
             ],
         ];
     }
