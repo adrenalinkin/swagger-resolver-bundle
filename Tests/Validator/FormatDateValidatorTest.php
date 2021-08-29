238060ff61f5eba4aac6f73a871f277d0303084b
Viktor Linkin <adrenalinkin@gmail.com>
2021-08-29 18:25:13 +0300

    Added unit tests for the `FormatDateValidator::validate`

--- Tests/Validator/FormatDateValidatorTest.php
+++ Tests/Validator/FormatDateValidatorTest.php
@@ -16,6 +16,7 @@ namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Validator;
 use EXSyst\Component\Swagger\Schema;
 use Linkin\Bundle\SwaggerResolverBundle\Validator\FormatDateValidator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 
 /**
  * @author Viktor Linkin <adrenalinkin@gmail.com>
@@ -33,7 +34,6 @@ class FormatDateValidatorTest extends TestCase
     {
         $this->sut = new FormatDateValidator();
     }
-
 
     /**
      * @dataProvider supportsDataProvider
@@ -59,6 +59,87 @@ class FormatDateValidatorTest extends TestCase
             'Success with right format' => [
                 'type' => self::FORMAT_DATE,
                 'expectedResult' => true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider failToPassValidationDataProvider
+     */
+    public function testFailToPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_DATE,
+            'pattern' => $pattern,
+        ]);
+
+        $this->expectException(InvalidOptionsException::class);
+
+        $this->sut->validate($schema, 'birthday', $value);
+    }
+
+    public function failToPassValidationDataProvider(): array
+    {
+        return [
+            'Fail when true value' => [
+                'pattern' => null,
+                'value' => true,
+            ],
+            'Fail when value with incorrect time pattern - number' => [
+                'pattern' => null,
+                'value' => '100',
+            ],
+            'Fail when value with incorrect time pattern - more than 2 digit' => [
+                'pattern' => null,
+                'value' => '19999-01-01',
+            ],
+            'Fail when pattern set and value can NOT convert into DateTime' => [
+                'pattern' => 'any-string-here', // TODO: should check format
+                'value' => '2020_05_05',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider canPassValidationDataProvider
+     */
+    public function testCanPassValidation(?string $pattern, $value): void
+    {
+        $schema = new Schema([
+            'format' => self::FORMAT_DATE,
+            'pattern' => $pattern,
+        ]);
+
+        $this->sut->validate($schema, 'birthday', $value);
+        self::assertTrue(true);
+    }
+
+    public function canPassValidationDataProvider(): array
+    {
+        return [
+            'Pass when null value' => [
+                'pattern' => null,
+                'value' => null,
+            ],
+            'Pass when empty string value' => [
+                'pattern' => null,
+                'value' => '',
+            ],
+            'Pass when empty zero value' => [ // TODO: should not pass validation
+                'pattern' => null,
+                'value' => '0',
+            ],
+            'Pass when false value' => [ // TODO: should not pass validation
+                'pattern' => null,
+                'value' => false,
+            ],
+            'Pass when value with correct date pattern' => [
+                'pattern' => null,
+                'value' => '2020-01-01',
+            ],
+            'Pass when pattern set and value can convert into DateTime' => [
+                'pattern' => 'any-string-here', // TODO: should check format
+                'value' => '2020/01/01',
             ],
         ];
     }
