8b15148d7df17dec7b050ab4a937a9f4e3c2e42c
Viktor Linkin <adrenalinkin@gmail.com>
2021-09-23 23:14:19 +0300

    Added new dataset for `OperationParameterMerger` test to merge header, query and body as reference.

--- Tests/Merger/OperationParameterMergerTest.php
+++ Tests/Merger/OperationParameterMergerTest.php
@@ -22,7 +22,7 @@ use Linkin\Bundle\SwaggerResolverBundle\Tests\Fixtures\FixturesLoader;
 use Linkin\Bundle\SwaggerResolverBundle\Tests\SwaggerFactory;
 use PHPUnit\Framework\TestCase;
 
-use function array_values;
+use function count;
 
 /**
  * @author Viktor Linkin <adrenalinkin@gmail.com>
@@ -47,7 +47,11 @@ class OperationParameterMergerTest extends TestCase
         $mergedSchema = $this->sut->merge($operation, $definitions);
         $mergedSchemaProperties = $mergedSchema->getProperties();
 
-        self::assertSame($expectedResult->getRequired(), array_values($mergedSchema->getRequired()));
+        self::assertCount(count($expectedResult->getRequired()), $mergedSchema->getRequired());
+
+        foreach ($expectedResult->getRequired() as $expectedRequired) {
+            self::assertArrayHasKey($expectedRequired, $mergedSchema->getRequired());
+        }
 
         foreach ($expectedResult->getProperties() as $name => $expectedParameter) {
             self::assertSame($mergedSchemaProperties->get($name)->toArray(), $expectedParameter->toArray());
@@ -57,24 +61,50 @@ class OperationParameterMergerTest extends TestCase
     public function mergeDataProvider(): iterable
     {
         $swagger = FixturesLoader::loadFromJson();
-        $customerGet = $swagger->getPaths()->get('/customers')->getOperation('get');
-        $expectedRequired = ['x-auth-token'];
-        $expectedProperties = [
-            'x-auth-token' => $this->getExpectedMergeResult($customerGet, 'x-auth-token', 'header'),
-            'page' => $this->getExpectedMergeResult($customerGet, 'page', 'query'),
-            'perPage' => $this->getExpectedMergeResult($customerGet, 'perPage', 'query'),
+        $operation = $swagger->getPaths()->get('/customers')->getOperation('get');
+        $requiredFields = ['x-auth-token'];
+        $mergedProperties = [
+            'x-auth-token' => $this->getOperationParameters($operation, 'x-auth-token', 'header'),
+            'page' => $this->getOperationParameters($operation, 'page', 'query'),
+            'perPage' => $this->getOperationParameters($operation, 'perPage', 'query'),
         ];
 
-        yield 'Merge header and query' => [
-            'parameters' => $customerGet,
+        yield 'get /customer - header+query' => [
+            'parameters' => $operation,
             'definitions' => $swagger->getDefinitions(),
-            'expectedResult' => SwaggerFactory::createSchemaDefinition($expectedProperties, $expectedRequired),
+            'expectedResult' => SwaggerFactory::createSchemaDefinition($mergedProperties, $requiredFields),
+        ];
+
+        $operation = $swagger->getPaths()->get('/customers')->getOperation('post');
+        $definition = $swagger->getDefinitions()->get('CustomerNew');
+        $requiredFields = (array) $definition->getRequired();
+        $requiredFields[] = 'x-auth-token';
+        $mergedProperties = $this->getDefinitionProperties($definition);
+        $mergedProperties['x-auth-token'] = $this->getOperationParameters($operation, 'x-auth-token', 'header');
+
+        yield 'post /customer - header+query+body as reference' => [
+            'parameters' => $operation,
+            'definitions' => $swagger->getDefinitions(),
+            'expectedResult' => SwaggerFactory::createSchemaDefinition($mergedProperties, $requiredFields),
         ];
     }
 
-    private function getExpectedMergeResult(Operation $operation, string $parameterName, string $parameterPlace): array
+    private function getDefinitionProperties(Schema $definition): array
     {
-        $result = $operation->getParameters()->get($parameterName, $parameterPlace)->toArray();
+        $properties = $definition->getProperties()->toArray();
+        $result = [];
+
+        foreach ($properties as $name => $property) {
+            $property['title'] = 'body';
+            $result[$name] = $property;
+        }
+
+        return $result;
+    }
+
+    private function getOperationParameters(Operation $operation, string $name, string $place): array
+    {
+        $result = $operation->getParameters()->get($name, $place)->toArray();
 
         $result['title'] = $result['in'];
         unset($result['in']);
