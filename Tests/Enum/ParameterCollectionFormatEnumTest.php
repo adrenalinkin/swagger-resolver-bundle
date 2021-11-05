6271cc2909e0f0fd1845690602e05c0a7292a18c
Viktor Linkin <adrenalinkin@gmail.com>
2021-11-05 17:20:52 +0300

    Cover all Enum's with unit tests.

---
+++ Tests/Enum/ParameterCollectionFormatEnumTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SwaggerResolverBundle package.
+ *
+ * (c) Viktor Linkin <adrenalinkin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Enum;
+
+use Linkin\Bundle\SwaggerResolverBundle\Enum\ParameterCollectionFormatEnum;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * @author Viktor Linkin <adrenalinkin@gmail.com>
+ */
+class ParameterCollectionFormatEnumTest extends TestCase
+{
+    public function testGetAll(): void
+    {
+        self::assertSame(ParameterCollectionFormatEnum::getAll(), [
+            'csv',
+            'ssv',
+            'tsv',
+            'pipes',
+            'multi',
+        ]);
+    }
+
+    public function testGetDelimiter(): void
+    {
+        self::assertSame(',', ParameterCollectionFormatEnum::getDelimiter('csv'));
+        self::assertSame(' ', ParameterCollectionFormatEnum::getDelimiter('ssv'));
+        self::assertSame("\t", ParameterCollectionFormatEnum::getDelimiter('tsv'));
+        self::assertSame('|', ParameterCollectionFormatEnum::getDelimiter('pipes'));
+        self::assertSame('&', ParameterCollectionFormatEnum::getDelimiter('multi'));
+    }
+
+    public function testFailGetDelimiterByUnexpectedFormat(): void
+    {
+        $this->expectException(RuntimeException::class);
+        ParameterCollectionFormatEnum::getDelimiter('_undefined_');
+    }
+}
---
+++ Tests/Enum/ParameterLocationEnumTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SwaggerResolverBundle package.
+ *
+ * (c) Viktor Linkin <adrenalinkin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Linkin\Bundle\SwaggerResolverBundle\Tests\Enum;
+
+use Linkin\Bundle\SwaggerResolverBundle\Enum\ParameterLocationEnum;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Viktor Linkin <adrenalinkin@gmail.com>
+ */
+class ParameterLocationEnumTest extends TestCase
+{
+    public function testGetAll(): void
+    {
+        self::assertSame(ParameterLocationEnum::getAll(), [
+            'body',
+            'formData',
+            'header',
+            'path',
+            'query',
+        ]);
+    }
+}
