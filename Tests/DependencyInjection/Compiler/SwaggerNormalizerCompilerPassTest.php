65150ccdf8f8ca3ccb4d1b8efb8bbb366f65e657
Viktor Linkin <adrenalinkin@gmail.com>
2022-07-22 00:26:52 +0300

    Added `LinkinSwaggerResolverExtensionTest`.

--- Tests/DependencyInjection/Compiler/SwaggerNormalizerCompilerPassTest.php
+++ Tests/DependencyInjection/Compiler/SwaggerNormalizerCompilerPassTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace DependencyInjection\Compiler;
+namespace Linkin\Bundle\SwaggerResolverBundle\Tests\DependencyInjection\Compiler;
 
 use Linkin\Bundle\SwaggerResolverBundle\Builder\SwaggerResolverBuilder;
 use Linkin\Bundle\SwaggerResolverBundle\DependencyInjection\Compiler\SwaggerNormalizerCompilerPass;
